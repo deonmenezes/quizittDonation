95caeb4f88a6fee91285d78a72fc0638beb2e5f1
YashChavanWeb <yashchavan4628@gmail.com>
2025-06-22 14:11:59 +0530

    backend changed

--- backend/index.js
+++ backend/index.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000;
 
 // Use CORS middleware
 app.use(cors({
-    // origin: 'https://quizitt-funds.vercel.app',
-    origin: 'http://localhost:8080',
+    origin: 'https://quizitt-funds.vercel.app',
+    // origin: 'http://localhost:8080',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
